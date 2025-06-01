d0a2f797c441c1ad17b4e6ce52d9cd0723cd3c1a
vkh <vl@d.d>
2025-06-02 02:58:00 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains: ['avatanplus.com','images.unsplash.com','www.google.com','img.freepik.com' , 'www.allprodad.com',"avatars.mds.yandex.net", "www.realsimple.com", "encrypted-tbn0.gstatic.com", "masterpiecer-images.s3.yandex.net"]
+        domains: ['api-tg-bot.simplifyapp.ru','avatanplus.com','images.unsplash.com','www.google.com','img.freepik.com' , 'www.allprodad.com',"avatars.mds.yandex.net", "www.realsimple.com", "encrypted-tbn0.gstatic.com", "masterpiecer-images.s3.yandex.net"]
     }
 };
 
